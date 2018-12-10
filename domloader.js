ff199f877d4c23067acd3ddcddf0c52048618df5
tmplink <45227137+tmplink@users.noreply.github.com>
2018-12-11 01:24:32 +0800

    更新到v1.2
    修复和优化UI问题，增加了debug参数。

--- domloader.js
+++ domloader.js
@@ -1,6 +1,6 @@
 /*!
  * domloader.js
- * v1.1
+ * v1.2
  * https://github.com/tmplink/domloader/
  * 
  * Licensed GPLv3 © TMPLINK STUDIO
@@ -9,14 +9,15 @@
 var domloader = {
     queue: [],
     progressbar: true,
-    total:0,
-    icon : false,
+    total: 0,
+    icon: false,
     id: 1,
+    debug: true,
 
     html: function (dom, path) {
-        this.id++;
-        console.log('Include::HTML::' + path);
-        this.queue.push(
+        domloader.id++;
+        domloader.log('Include::HTML::' + path);
+        domloader.queue.push(
                 function () {
                     $.get(path, function (response) {
                         $(dom).replaceWith(response);
@@ -27,8 +28,8 @@ var domloader = {
     },
 
     css: function (path) {
-        console.log('Include::CSS::' + path);
-        this.queue.push(
+        domloader.log('Include::CSS::' + path);
+        domloader.queue.push(
                 function () {
                     domloader.id++;
                     $('head').append("<link async id=\"domloader_" + domloader.id + "\" rel=\"stylesheet\" href=\"" + path + "\" >\n");
@@ -40,8 +41,8 @@ var domloader = {
     },
 
     js: function (path) {
-        console.log('Include::JS::' + path);
-        this.queue.push(
+        domloader.log('Include::JS::' + path);
+        domloader.queue.push(
                 function () {
                     $.get(path, function (response) {
                         domloader.id++;
@@ -53,27 +54,27 @@ var domloader = {
     },
 
     load: function (src) {
-        if (typeof (src) !== 'undefined') {
-            var percent = Math.ceil((this.total-this.queue.length)/this.total*100);
-            $('.curRate').animate({'width': percent+'%'},"fast");
-            console.log("Loaded::" + src);
-        }
-        if (this.queue.length === 0) {
-            if (this.progressbar === false) {
-                $('body').css('overflow','');
-                $('#domloader_loading_bg').fadeOut();
-                $('#domloader_loading_show').fadeOut();
+        $('body').css('overflow', 'hidden');
+        if (domloader.queue.length === 0) {
+            if (domloader.progressbar === false) {
+                $('#domloader_loading_show').fadeOut(100);
+                $('#domloader_loading_bg').fadeOut(100);
+                $('body').css('overflow', '');
             }
         } else {
-            if (this.progressbar) {
-                this.total = this.queue.length;
-                this.progressbar = false;
-                $('body').css('overflow','hidden');
+            if (domloader.progressbar) {
+                domloader.total = domloader.queue.length;
+                domloader.progressbar = false;
                 $('#domloader_loading_bg').show();
                 $('#domloader_loading_show').show();
             }
         }
-        var fn = this.queue.shift();
+        if (typeof (src) !== 'undefined') {
+            var percent = Math.ceil((this.total - this.queue.length) / this.total * 100);
+            $('.curRate').animate({'width': percent + '%'}, 100);
+            domloader.log("Loaded::" + src);
+        }
+        var fn = domloader.queue.shift();
         if (typeof (fn) === 'function') {
             fn();
         }
@@ -81,15 +82,22 @@ var domloader = {
 
     init: function () {
         window.onload = function () {
+            $('head').append('<style>*, ::after, ::before {box-sizing: border-box;}</style>');
             $('body').append('<div id="domloader_loading_bg" style="position: fixed;top: 0;left: 0;width: 100%;height: 100%;background: white;z-index: 15000;"></div>');
             $('body').append('<div id="domloader_loading_show" style="color: black;;z-index: 15001;width: 200px; height: 200px;position: absolute; left: 0;top: 0;right: 0;bottom: 0; margin: auto;"></div>');
-            if(domloader.icon!==false){
-                $('#domloader_loading_show').append('<div style="text-align:center;margin-bottom:20px;"><img src="'+domloader.icon+'" /></div>');
+            if (domloader.icon !== false) {
+                $('#domloader_loading_show').append('<div style="text-align:center;margin-bottom:20px;"><img src="' + domloader.icon + '" style="vertical-align: middle;border-style: none;"/></div>');
             }
             $('#domloader_loading_show').append('<div class="progress round-conner"><div class="curRate round-conner"></div></div>');
-            $('#domloader_loading_show').append('<style>.progress {width: 200px;background: #ddd;}.curRate {width: 0%;background: #f30;}.round-conner {height: 10px;border-radius: 15px;}</style>');
-            console.log('Page ready.Domloader start.');
+            $('#domloader_loading_show').append('<style>.progress {width: 180px;background: #ddd;margin-right:auto;margin-left:auto;}.curRate {width: 0%;background: #f30;}.round-conner {height: 10px;border-radius: 15px;}</style>');
+            domloader.log('Page ready.Domloader start.');
             domloader.load();
+        }
+    },
+
+    log: function (msg) {
+        if (this.debug) {
+            console.log(msg);
         }
     }
 }
